17f6964deb5d7b475469f08626bc7006b23aa8e3
t7 <>
2024-08-23 09:51:55 +0900

    delete 不要な箇所を削除

---
+++ lib/presentation/screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Screen extends StatefulWidget {
+  const Screen({super.key});
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello, World!',
+        ),
+      ),
+    );
+  }
+}
